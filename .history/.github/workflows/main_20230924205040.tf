4ef37898bd1511d79107e73ea03d82fbcdc79c8c
kingphillips <phillipsidowu3@gmail.com>
2023-09-24 20:51:40 -0500

    fourth commit

--- .history/.github/workflows/main_20230924205040.tf
+++ .history/.github/workflows/main_20230924205040.tf
@@ -1,6 +1,6 @@
 #provider "aws" {}
 
 #resource "aws_vpc" "test" {
-    #cidr ="10.0.0.0/16" 
+    cidr ="10.0.0.0/16" 
   
-#}
+}
--- .history/.github/workflows/main_20230924205046.tf
+++ .history/.github/workflows/main_20230924205046.tf
@@ -3,4 +3,4 @@
 #resource "aws_vpc" "test" {
     #cidr ="10.0.0.0/16" 
   
-#}
+}
